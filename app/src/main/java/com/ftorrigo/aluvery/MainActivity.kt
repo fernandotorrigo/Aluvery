a04af7111d8e489c76888b6d6d6f8adf57e09bc2
Fernando Torrigo <torrigofernando@gmail.com>
2024-03-18 22:57:25 +0000

    Implementing product item with section

--- app/src/main/java/com/ftorrigo/aluvery/MainActivity.kt
+++ app/src/main/java/com/ftorrigo/aluvery/MainActivity.kt
@@ -4,11 +4,38 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.ftorrigo.aluvery.ui.theme.AluveryTheme
+import com.ftorrigo.aluvery.ui.theme.Purple40
+import com.ftorrigo.aluvery.ui.theme.PurpleGrey40
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,7 +43,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             AluveryTheme {
                 Surface {
-                    MyFirstComposable()
+                    ProductSection()
                 }
             }
         }
@@ -24,22 +51,78 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MyFirstComposable(){
-    Text(text = "My first Composablee")
-    Text(text = "My first Composableee Maior")
+fun ProductItem() {
+    Surface(
+        Modifier.padding(8.dp),
+        shape = RoundedCornerShape(15.dp),
+        shadowElevation = 4.dp
+    ) {
+        Column(
+            Modifier
+                .heightIn(250.dp, 300.dp)
+                .width(200.dp)
+        ) {
+
+            val imageSize = 100.dp
+            Box(
+                modifier = Modifier
+                    .height(imageSize)
+                    .background(
+                        brush = Brush.horizontalGradient(
+                            colors = listOf(
+                                Purple40,
+                                PurpleGrey40
+                            )
+                        )
+                    )
+                    .fillMaxWidth()
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_launcher_background),
+                    contentDescription = null,
+                    Modifier
+                        .size(imageSize)
+                        .offset(y = imageSize / 2)
+                        .clip(shape = CircleShape)
+                        .align(Alignment.BottomCenter)
+                )
+            }
+            Spacer(modifier = Modifier.height(imageSize / 2))
+            Column(Modifier.padding(16.dp)) {
+                Text(
+                    text = LoremIpsum(50).values.first(),
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight(700),
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis
+                )
+                Text(
+                    text = "R$ 14,99",
+                    Modifier.padding(top = 8.dp),
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight(400),
+                )
+            }
+        }
+
+    }
 }
 
-@Preview(
-    name =  "TextPreview",
-    showBackground = true,
-    uiMode = Configuration.UI_MODE_NIGHT_YES
 
-)
 @Composable
-fun MyFirstComposablePreview(){
-    AluveryTheme {
-        Surface {
-            MyFirstComposable()
+fun ProductSection() {
+    Column {
+        Text(text = "Promoções")
+        Row {
+            ProductItem()
+            ProductItem()
+            ProductItem()
         }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun ProductItemPreview() {
+    ProductSection()
 }
