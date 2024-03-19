0eccb5bc63aa322409f57ebbf08fdc53bd8be1c0
Fernando Torrigo <torrigofernando@gmail.com>
2024-03-19 23:07:54 +0000

    Inserting Images and new BigDecimal Extension

--- app/src/main/java/com/ftorrigo/aluvery/MainActivity.kt
+++ app/src/main/java/com/ftorrigo/aluvery/MainActivity.kt
@@ -1,11 +1,9 @@
 package com.ftorrigo.aluvery
 
-import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
@@ -30,16 +28,19 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.ftorrigo.aluvery.extensions.toBrazilianCurrency
+import com.ftorrigo.aluvery.model.Product
 import com.ftorrigo.aluvery.ui.theme.AluveryTheme
 import com.ftorrigo.aluvery.ui.theme.Purple40
 import com.ftorrigo.aluvery.ui.theme.PurpleGrey40
+import java.math.BigDecimal
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -55,7 +56,7 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun ProductItem() {
+fun ProductItem(product: Product) {
     Surface(
         shape = RoundedCornerShape(15.dp),
         shadowElevation = 4.dp
@@ -81,26 +82,27 @@ fun ProductItem() {
                     .fillMaxWidth()
             ) {
                 Image(
-                    painter = painterResource(id = R.drawable.ic_launcher_background),
+                    painter = painterResource(id = product.image),
                     contentDescription = null,
                     Modifier
                         .size(imageSize)
                         .offset(y = imageSize / 2)
                         .clip(shape = CircleShape)
-                        .align(Alignment.BottomCenter)
+                        .align(Alignment.BottomCenter),
+                    contentScale = ContentScale.Crop
                 )
             }
             Spacer(modifier = Modifier.height(imageSize / 2))
             Column(Modifier.padding(16.dp)) {
                 Text(
-                    text = LoremIpsum(50).values.first(),
+                    text = product.name,
                     fontSize = 18.sp,
                     fontWeight = FontWeight(700),
                     maxLines = 2,
                     overflow = TextOverflow.Ellipsis
                 )
                 Text(
-                    text = "R$ 14,99",
+                    text = product.price.toBrazilianCurrency(),
                     Modifier.padding(top = 8.dp),
                     fontSize = 14.sp,
                     fontWeight = FontWeight(400),
@@ -132,9 +134,28 @@ fun ProductSection() {
             Arrangement.spacedBy(16.dp)
         ) {
             Spacer(Modifier)
-            ProductItem()
-            ProductItem()
-            ProductItem()
+            ProductItem(
+                Product(
+                    name = "Hamburguer",
+                    image = R.drawable.hamburguer,
+                    price = BigDecimal("18.90")
+                )
+            )
+            ProductItem(
+                Product(
+                    name = "Pizza",
+                    image = R.drawable.pizza,
+                    price = BigDecimal("34.90")
+                )
+            )
+            ProductItem(
+                Product(
+                    name = "batata frita",
+                    image = R.drawable.batata_frita,
+                    price = BigDecimal("12.90")
+                )
+            )
+
             Spacer(Modifier)
         }
     }
---
+++ app/src/main/java/com/ftorrigo/aluvery/model/Product.kt
@@ -0,0 +1,10 @@
+package com.ftorrigo.aluvery.model
+
+import androidx.annotation.DrawableRes
+import java.math.BigDecimal
+
+class Product(
+    val name: String,
+    val price: BigDecimal,
+    @DrawableRes val image: Int
+)
