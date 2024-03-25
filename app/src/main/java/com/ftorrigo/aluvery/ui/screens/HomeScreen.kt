7394259346811683761e1017f0195fd07c0f8dbf
Fernando Torrigo <torrigofernando@gmail.com>
2024-03-25 18:34:29 +0000

    implementing TextField to Search

--- app/src/main/java/com/ftorrigo/aluvery/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/ftorrigo/aluvery/ui/screens/HomeScreen.kt
@@ -2,12 +2,25 @@ package com.ftorrigo.aluvery.ui.screens
 
 import ProductSection
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -19,30 +32,57 @@ import com.ftorrigo.aluvery.ui.theme.AluveryTheme
 fun HomeScreen(
     sections: Map<String, List<Product>>
 ) {
-    LazyColumn(
-        Modifier
-            .fillMaxSize(),
-        verticalArrangement = Arrangement.spacedBy(16.dp),
-        contentPadding = PaddingValues(vertical = 16.dp)
-    ) {
-        item {
-            Spacer(Modifier)
-        }
-        for (section in sections) {
-            val title = section.key
-            val products = section.value
-            item {
-                ProductSection(
-                    title = title,
-                    productsList = products
-                )
-            }
 
-        }
-        item {
-            Spacer(Modifier)
+    Column {
+        var text by remember { mutableStateOf("") }
+
+        OutlinedTextField(
+            value = text,
+            onValueChange = { text = it },
+            Modifier
+                .padding(
+                    start = 16.dp,
+                    top = 16.dp,
+                    end = 16.dp,
+                )
+                .fillMaxWidth(),
+            shape = RoundedCornerShape(100),
+            leadingIcon = {
+                Icon(Icons.Default.Search, contentDescription = "Icone search")
+            },
+            label = {
+                Text(text = "Produto")
+            },
+            placeholder = {
+                Text(text = "O que voçê espera?")
+            }
+        )
+        LazyColumn(
+            Modifier
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            contentPadding = PaddingValues(vertical = 16.dp)
+        ) {
+            item {
+                Spacer(Modifier)
+            }
+            for (section in sections) {
+                val title = section.key
+                val products = section.value
+                item {
+                    ProductSection(
+                        title = title,
+                        productsList = products
+                    )
+                }
+
+            }
+            item {
+                Spacer(Modifier)
+            }
         }
     }
+
 }
 
 @Preview(showSystemUi = true)
